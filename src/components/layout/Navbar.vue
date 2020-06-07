8abdc9865e0733a26767cfcab2dbc89c7bb989d5
Rafael Sampaio Tavares <rafael_killua@msn.com>
2020-06-06 23:26:47 -0300

    navbar

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -4,13 +4,13 @@
     <transition name="slide-down">
       <ul class="dropdown" v-if="showMobile" @click="showMobile = false">
         <li class="dropdown-item">
-          <router-link to="/">Empresas</router-link>
+          <router-link to="/" exact-active-class="dropdown-item-active">Empresas</router-link>
         </li>
         <li class="dropdown-item">
-          <router-link to="/cadastrar">Cadastrar minha empresa</router-link>
+          <router-link to="/cadastrar" exact-active-class="dropdown-item-active">Cadastrar minha empresa</router-link>
         </li>
         <li class="dropdown-item">
-          <router-link to="/quem-somos">Quem somos</router-link>
+          <router-link to="/quem-somos" exact-active-class="dropdown-item-active">Quem somos</router-link>
         </li>
       </ul>
     </transition>
@@ -18,19 +18,19 @@
       <router-link to="/">
         <img
           class="logo"
-          src="https://www.vhv.rs/dpng/d/201-2010185_thumb-image-placeholder-image-logo-svg-hd-png.png"
+          src="https://via.placeholder.com/350x150?text=Divulga"
           @click="showMobile = false"
         />
       </router-link>
       <ul class="nav">
         <li>
-          <router-link class="link" to="/">Empresas</router-link>
+          <router-link class="link" exact-active-class="link-active" to="/">Empresas</router-link>
         </li>
         <li>
-          <router-link class="link" to="/cadastrar">Cadastrar minha empresa</router-link>
+          <router-link class="link" exact-active-class="link-active" to="/cadastrar">Cadastrar minha empresa</router-link>
         </li>
         <li>
-          <router-link class="link" to="/quem-somos">Quem somos</router-link>
+          <router-link class="link" exact-active-class="link-active" to="/quem-somos">Quem somos</router-link>
         </li>
       </ul>
       <menu-icon class="menu-toggle" @click="showMobile = !showMobile" />
@@ -76,11 +76,13 @@ export default {
     @apply hidden
 
 .link
-  @apply font-bold ml-4 text-white
+  @apply font-bold ml-2 text-white
   &:hover
     @apply text-gray-300
   &:active
     @apply text-gray-500
+  &-active
+    @apply border border-white rounded-lg p-2
 
 .backdrop
   @apply fixed w-screen h-screen top-0 left-0 bg-transparent opacity-75
@@ -89,7 +91,7 @@ export default {
   top: 4rem
   @apply absolute w-full bg-white shadow-lg
   &-item
-    @apply w-full flex border-b-2 py-1
+    @apply w-full flex border-b-2 py-1 px-6
     &:first-of-type
       @apply border-t-2
     &:hover
@@ -98,6 +100,8 @@ export default {
       @apply bg-gray-400
     &> a
       @apply w-full h-full
+    &-active
+      @apply text-primary
 
 .slide-down-enter-active, .slide-down-leave-active
   transition: all .3s
